a24c5af16f7fded8f8a7d33da967513b9ff215cb
bi305 <72016671+bi305@users.noreply.github.com>
2021-12-03 13:16:52 +0500

    layout  size  is  set now  for  all  pages

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
       <Content style={{ margin: "24px 16px 0" }}>
         <div
           className="site-layout-background"
-          style={{ padding: 24, minHeight: 360 }}
+          style={{ padding: 24, minHeight: "100vh" }}
         >
           content
         </div>
